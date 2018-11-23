9c55618f6baf2da5cb21e320910fd81a89ef79bb
Kristen Peck <kpeck71@gmail.com>
2018-11-23 15:39:25 -0500

    add foreman and get working

---
+++ public/precache-manifest.0cfd2fd522c6747f3185b156861d2af0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b0b113041fe417d02476",
+    "url": "/static/js/main.b0b11304.chunk.js"
+  },
+  {
+    "revision": "a4fbdf2e987e68ddd3df",
+    "url": "/static/js/1.a4fbdf2e.chunk.js"
+  },
+  {
+    "revision": "b0b113041fe417d02476",
+    "url": "/static/css/main.dfcf63eb.chunk.css"
+  },
+  {
+    "revision": "2a3ce4b5120a1fff30dce385ec916d8b",
+    "url": "/index.html"
+  }
+];
